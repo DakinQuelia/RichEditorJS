b999136bd5e2ed1924b4f30fd3897566ef129e62
Dakin Quelia <dakinquelia@gmail.com>
2025-01-21 10:48:29 +0100

    Mise à jour

--- js/editor/context.js
+++ js/editor/context.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*****************************************
 *	Editeur de texte
 *	Auteur 		: Dakin Quelia
@@ -18,25 +17,4 @@ var context = [
     // ------------------------------------------------------------------------------------------------------------------------------------------------
 ];
 
-=======
-/*****************************************
-*	Editeur de texte
-*	Auteur 		: Dakin Quelia
-*	Version 	: 1.0.0. 
-*****************************************/
-import config from './config.js';
-import Editor from './editor.js';
-
-var context = [
-    { text: 'Copier', id: 'copy', type: 'action', enabled: true, icon: 'fa-copy', url: '', action: () => { Editor.ContextCopy() } },
-    { text: 'Couper', id: 'cut', type: 'action', enabled: true, icon: 'fa-cut', url: '', action: () => { Editor.Action('cut') } },
-    { text: 'Coller', id: 'paste', type: 'action', enabled: true, icon: 'fa-paste', url: '', action: () => { Editor.Action('paste') } },
-    { text: 'Supprimer', id: 'delete', type: 'action', enabled: false, icon: 'fa-times', url: '', action: () => { Editor.Action('paste') } },
-    // ------------------------------------------------------------------------------------------------------------------------------------------------
-    //{ text: 'Séparateur', id: 'separator', type: 'separator' },
-    //{ text: 'A propos', id: 'about', type: 'link', enabled: true, icon: 'fa-info-circle', url: '', action: (event) => { Editor.About(event) } },
-    // ------------------------------------------------------------------------------------------------------------------------------------------------
-];
-
->>>>>>> 1782344523dd73ab918ef419c89b3bd87bb478c6
 export default context;
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*****************************************
 *	Fichier principal
 *	Auteur 		: Dakin Quelia
@@ -10,17 +9,4 @@ import Editor from './editor/editor.js';
 {
     // On lance l'éditeur
     new Editor().Init();
-=======
-/*****************************************
-*	Fichier principal
-*	Auteur 		: Dakin Quelia
-*	Version 	: 1.0.0. 
-*****************************************/
-import Editor from './editor/editor.js';
-
-(function() 
-{
-    // On lance l'éditeur
-    new Editor().Init();
->>>>>>> 1782344523dd73ab918ef419c89b3bd87bb478c6
 })();	
